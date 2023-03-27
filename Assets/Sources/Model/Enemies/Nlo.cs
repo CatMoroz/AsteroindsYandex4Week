01dbd9d3b019e65b8f6d0d642e6e80e69e7523e1
CatMoroz <116497717+CatMoroz@users.noreply.github.com>
2023-03-27 12:17:26 +0300

    Congratulations

---
+++ Assets/Sources/Model/Enemies/Nlo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Asteroids.Model
+{
+    public class Nlo : Enemy
+    {
+        private const float MinDistance = .001f;
+        private static List<Nlo> Nlos { get; } = new List<Nlo>();
+
+        private readonly float _speed;
+        public int Team { get; }
+
+        private static List<Nlo> GetOppositeTeam(int team) => Nlos.Where(nlo => nlo.Team != team).ToList();
+
+        public Nlo(Vector2 position, float speed, int team) : base(position, 0)
+        {
+            Team = team;
+            _speed = speed;
+            Nlos.Add(this);
+            Destroying += () => Nlos.Remove(this);
+        }
+
+        public override void Update(float deltaTime)
+        {
+            var oppositeTeam = GetOppositeTeam(Team);
+            if (!oppositeTeam.Any()) 
+            {
+                return;
+            }
+            var target = oppositeTeam.Aggregate((targetA, targetB) => 
+                Vector2.Distance(Position, targetB.Position) < Vector2.Distance(Position, targetA.Position)
+                    ? targetB
+                    : targetA);
+
+            if (Vector2.Distance(Position, target.Position) < MinDistance)
+            {
+                Destroy();
+                target.Destroy();
+                return;
+            }
+            Vector2 nextPosition = Vector2.MoveTowards(Position, target.Position, _speed * deltaTime);
+            MoveTo(nextPosition);
+            LookAt(target.Position);
+        }
+
+        private void LookAt(Vector2 point)
+        {
+            Rotate(Vector2.SignedAngle(Quaternion.Euler(0, 0, Rotation) * Vector3.up, (Position - point)));
+        }
+    }
+}
---
+++ Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            if (_init.Ship.Heathe>1)
+            {
+                _init.Ship.Heathe--;
+            }
+            else
+            {
+                _init.Ship.Destroy();
+                _init.DisableShip();
+            }
+        }
+    }
+}
